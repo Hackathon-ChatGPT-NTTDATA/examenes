29e331a5daad12a28d03b01a9ba1aa16f4b75203
joffre <joffre.hermosilla@gmail.com>
2023-07-12 03:43:51 -0500

    chore[0] primera configuracion

---
+++ examenes/src/main/java/hackathon/nttdata/coderpath/examenes/ExamenesApplication.java
@@ -0,0 +1,13 @@
+package hackathon.nttdata.coderpath.examenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenesApplication.class, args);
+	}
+
+}
---
+++ examenes/src/test/java/hackathon/nttdata/coderpath/examenes/ExamenesApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.nttdata.coderpath.examenes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
